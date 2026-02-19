30b17f18668e77db69453e78bf08a71fb9996f12
TangoCode99 <jnham7@gmail.com>
2026-02-18 20:49:05 -0500

    Responsive header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/header.css";
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
     return (
         <header className="app-header">
             <div id="navbar">
-                <div id="wrapper">
-                    <nav>
-                        <ul>
-                            <li><Link to="/">About</Link></li>
-                            <li><Link to="/photos">Latest Photos</Link></li>
-                        </ul>
-                    </nav>
-                </div>
                 <div className="app-name">
                     <div className="main-name">
                         Tango Nham
@@ -21,6 +23,25 @@ export default function Header() {
                     <div className="subname">
                         Developer / Photographer
                     </div>
+                </div>
+
+                <button
+                    className={`hamburger ${menuOpen ? 'active' : ''}`}
+                    onClick={toggleMenu}
+                    aria-label="Toggle menu"
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+
+                <div id="wrapper" className={menuOpen ? 'open' : ''}>
+                    <nav>
+                        <ul>
+                            <li><Link to="/" onClick={closeMenu}>About</Link></li>
+                            <li><Link to="/photos" onClick={closeMenu}>Latest Photos</Link></li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </header>
